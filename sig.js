a19af91fbf5685b0fb30c4c0b2a661dba558a70b
Courtney Ross <134150039+courtneyleora@users.noreply.github.com>
2024-04-17 18:10:05 -0400

    fixed validation form

--- sig.js
+++ sig.js
@@ -26,34 +26,33 @@ const addSignature = (event) => {
   count = count + 1;
   const newCount = document.createElement('p');
   newCount.id = 'counter';
-  newCount.textContent = `🖊️ ${count} people have signed the petition and supports this cause.`;
+  newCount.textContent = `🖊️ ${count} people have signed the petition and support this cause.`;
   signaturesSection.appendChild(newCount);
+};
 
-
-}
 let count = 3;
+
 // Add click event listener to the "Sign Now" button
 // signNowButton.addEventListener('click',addSignature);
 
-  // TODO: Remove the click event listener that calls addSignature()
+// TODO: Remove the click event listener that calls addSignature()
 
 // TODO: Complete validation form
-
 const validateForm = () => {
-
   let containsErrors = false;
-
-  var petitionInputs = document.getElementById("sign-petition").elements;
-  // TODO: Loop through all inputs
+  const petitionInputs = document.getElementById("sign-petition").elements;
+  
+  // Loop through all inputs
   for (let i = 0; i < petitionInputs.length; i++) {
     if (petitionInputs[i].value.length < 2) {
       containsErrors = true;
       petitionInputs[i].classList.add('error');
-    }else {
+    } else {
       petitionInputs[i].classList.remove('error');
     }
   }
-  if (containsErrors == false) {
+  
+  if (!containsErrors) {
     addSignature();
   
     // Clear input fields and reset containsErrors
@@ -62,13 +61,7 @@ const validateForm = () => {
     }
     containsErrors = false;
   }
-  
-    }
-  // TODO: Validate the value of each input
+};
 
-
-
-  // TODO: Call addSignature() and clear fields if no errors
-
-
+// Add event listener to the "Sign Now" button for form validation
 signNowButton.addEventListener('click', validateForm);
